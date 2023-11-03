b6577317b3931688527493311bf6857ef0f8891d
Marcelo Ferraz <carvalhoferrazmarcelo@gmail.com>
2023-11-03 16:00:03 -0300

    Updated grantBulkHubAccess.py with docstrings for the 2 main functions

--- grantBulkHubAccess.py
+++ grantBulkHubAccess.py
@@ -160,6 +160,28 @@ def move_to_new_hub(env, orgId, associate, userName):
 
 
 def apply_changes(env, orgId, hubsList, allFleets, associate, userName):
+    """
+    This function manages the assignment and updating of associate access to hubs within fleets based on the availability of fleets and hubs. It performs necessary updates and creations, and communicates the results to the user.
+
+    Parameters:
+    - env: The environment where the fleet organization operates.
+    - orgId: The organization ID within the chosen environment.
+    - hubsList: A list of hubs to which the associate should have access.
+    - allFleets: A list of all fleets within the selected environment and organization.
+    - associate: The associate whose access is being managed.
+    - userName: The name of the user making the changes.
+
+    This function performs the following logical steps:
+
+    1. Retrieve the names and IDs of hubs from the provided hubsList.
+    2. Search for a fleet that has the same hubs as the associate's access.
+    3. If a fleet with the correct hubs already exists, update the associate's data with that fleet.
+    4. If the associate already has a fleet, check if other associates use the same fleet.
+        - If no other associate uses the same fleet, update the existing fleet with the new hubs.
+        - If other associates use the same fleet, create a new fleet with the provided hubs.
+    5. If the associate doesn't have a fleet, create a new fleet with the provided hubs and associate it with the associate.
+
+    """
     hubsNames = [h.get('name') for h in hubsList]
     hubsIds = [h.get('id') for h in hubsList]
 
@@ -209,30 +231,24 @@ def apply_changes(env, orgId, hubsList, allFleets, associate, userName):
 
             update_associate(env, associate, userName)
 
-# How the script works:
-# - Get associate IDs from 'associates.txt,' with each new line representing a new associate ID. No need for special formatting.
-# - Use multithreading to search for associate IDs and save them in the ASSOCIATES array when found.
-# - Interactively ask each associate about their hub access needs, one hub at a time. Press Enter with a blank input to continue.
-# - Check if associates need to keep their current hub access:
-#   - If yes:
-#     - Add new hubs to the associate's hub list.
-#     - Look for a fleet that already contains these hubs.
-#       - If a fleet is found, update the associate's data with the fleet ID.
-#       - If none exists, check if other associates use the same fleet.
-#         - If no one does, update the fleet.
-#         - If others do, create a new fleet with old and new hubs.
-#   - If no:
-#     - Search for a fleet with the new hubs.
-#       - If a fleet is found, update the associate's data with the fleet ID.
-#       - If none is found, check if other associates use the same fleet.
-#         - If no one does, update the fleet.
-#         - If others do, create a new fleet with old and new hubs.
-#
-# Additionally, manage driver and sorter relocations as needed:
-# - Determine the new hub.
-# - Modify the associate's hubId and locationId.
-# - Remove any references to the previous 'fleetId,' 'preferredVehicle,' and 'preferredRoute' before updating the associate's data."
+
 def main():
+    """
+    The main function of the script for managing associate access to hubs within fleets and handling driver/sorter associates differently. The script allows users to make decisions regarding access and updates based on prompts and user input.
+
+    This function performs the following logical steps:
+
+    1. Prompt the user for their name.
+    2. Select an environment (env) using a utility function.
+    3. Select an organization (orgId) within the chosen environment.
+    4. Read 'associates.txt' to retrieve Associate IDs.
+    5. Launch a thread pool for parallel processing of associate data.
+    6. For each Associate ID, fetch associate details in parallel threads.
+    7. Search for all fleets within the selected environment and organization.
+    8. Iterate through each associate and manage their access to hubs or perform actions based on their role.
+
+    Ensure that the necessary utility functions, input/output mechanisms, and dependencies are properly configured for the script to execute successfully.
+    """
     userName = input("What is your name?\n> ")
     env = utils.select_env()
     orgId = utils.select_org(env)
@@ -276,8 +292,28 @@ def main():
                 if answer == "Y":
                     move_to_new_hub(env, orgId, associate, userName)
 
-                print("Finishing script...")
-                sys.exit()
-
 
 main()
+# How the script works:
+# - Get associate IDs from 'associates.txt,' with each new line representing a new associate ID. No need for special formatting.
+# - Use multithreading to search for associate IDs and save them in the ASSOCIATES array when found.
+# - Interactively ask each associate about their hub access needs, one hub at a time. Press Enter with a blank input to continue.
+# - Check if associates need to keep their current hub access:
+#   - If yes:
+#     - Add new hubs to the associate's hub list.
+#     - Look for a fleet that already contains these hubs.
+#       - If a fleet is found, update the associate's data with the fleet ID.
+#       - If none exists, check if other associates use the same fleet.
+#         - If no one does, update the fleet.
+#         - If others do, create a new fleet with old and new hubs.
+#   - If no:
+#     - Search for a fleet with the new hubs.
+#       - If a fleet is found, update the associate's data with the fleet ID.
+#       - If none is found, check if other associates use the same fleet.
+#         - If no one does, update the fleet.
+#         - If others do, create a new fleet with old and new hubs.
+#
+# Additionally, manage driver and sorter relocations as needed:
+# - Determine the new hub.
+# - Modify the associate's hubId and locationId.
+# - Remove any references to the previous 'fleetId,' 'preferredVehicle,' and 'preferredRoute' before updating the associate's data."
