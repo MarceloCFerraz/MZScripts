9af559587aac580d1946cecbfc004da16aee07f3
Marcelo Ferraz <carvalhoferrazmarcelo@gmail.com>
2024-07-19 18:22:37 -0300

    add reason column and remove shoprite and hn

--- generateWeeklyReport.py
+++ generateWeeklyReport.py
@@ -32,6 +32,7 @@ def filter_tickets(
     tickets = (
         unify_account_names(tickets, account_name)
         .with_columns(
+            pl.lit("").alias("Reason"),
             pl.col("State")
             .replace("Closed", "Solved")
             .replace("Cancelled", "Solved")
@@ -42,7 +43,6 @@ def filter_tickets(
             .replace("Open", "Active")
             .replace("On Hold", "Pending")
             .replace("Awaiting Info", "Pending"),
-            pl.col("Contact").fill_null(pl.col("Opened by") if use_opened_by else "-"),
         )
         .filter(pl.col("Account") == account_name)
         .select(
@@ -53,15 +53,27 @@ def filter_tickets(
             pl.col("Opened"),
             pl.col("Updated"),
             pl.col("Assignment group"),
+            pl.col("Reason"),
         )
         .sort(["State", "Number"])
         .rename({"Number": "Case Number"})
         .rename({"Contact": "Requester"})
         .rename({"Opened": "Opened At"})
         .rename({"Updated": "Last Updated At"})
+        .with_columns()
     )
 
     return tickets
+
+
+def read_excel(file_name: str) -> pl.DataFrame:
+    try:
+        df = pl.read_excel(file_name)
+    except Exception:
+        print(f"Error reading '{file_name}'")
+        return pl.DataFrame()
+
+    return df
 
 
 def merge_files(
@@ -71,23 +83,31 @@ def merge_files(
     closed_incidents_file: str,
 ):
     # read excel files
-    open_cases = pl.read_excel(open_cases_file)
-    open_incidents = (
-        pl.read_excel(open_incidents_file)
-        .rename({"Company": "Account"})
-        .rename({"Caller": "Contact"})
-    )
-    closed_cases = pl.read_excel(closed_cases_file)
-    closed_incidents = (
-        pl.read_excel(closed_incidents_file)
-        .rename({"Company": "Account"})
-        .rename({"Caller": "Contact"})
-    )
+    open_cases = read_excel(open_cases_file)
+    open_incidents = read_excel(open_incidents_file)
+    closed_cases = read_excel(closed_cases_file)
+    closed_incidents = read_excel(closed_incidents_file)
+
+    valid_tickets = []
+
+    if not open_cases.is_empty():
+        valid_tickets.append(open_cases)
+
+    if not closed_cases.is_empty():
+        valid_tickets.append(closed_cases)
+
+    if not open_incidents.is_empty():
+        valid_tickets.append(
+            open_incidents.rename({"Company": "Account", "Caller": "Contact"})
+        )
+
+    if not closed_incidents.is_empty():
+        valid_tickets.append(
+            closed_incidents.rename({"Company": "Account", "Caller": "Contact"})
+        )
 
     # create a dataframe with all the tickets
-    all_tickets = pl.concat(
-        [open_cases, open_incidents, closed_cases, closed_incidents]
-    )
+    all_tickets = pl.concat(valid_tickets)
 
     return all_tickets
 
@@ -126,7 +146,7 @@ if __name__ == "__main__":
         open_incidents_file="Incidents - All Open.xlsx",
         closed_cases_file="Cases - All Closed.xlsx",
         closed_incidents_file="Incidents - All Closed.xlsx",
-        accounts=["Staples", "Endeavour", "Harvey Norman", "ShopRite"],
+        accounts=["Staples", "Endeavour"],  # , "Harvey Norman", "ShopRite"],
     )
     end = datetime.now()
     print(f"Time taken: {end - start}")
