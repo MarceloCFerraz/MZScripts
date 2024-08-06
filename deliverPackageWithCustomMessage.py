293d897bb87a0d3e5ce9ddee88040b19b733ef95
Marcelo Ferraz <carvalhoferrazmarcelo@gmail.com>
2024-08-06 12:15:59 -0300

    feat: add new script to mark a single package delivered with custom message

---
+++ deliverPackageWithCustomMessage.py
@@ -0,0 +1,42 @@
+from concurrent.futures import ThreadPoolExecutor
+
+from utils import packages, utils
+
+
+def main(env: str, org_id: str, key_type: str, key: str, notes: str | None):
+    pkgs = []
+
+    pkgs = packages.get_package_details(env, org_id, key_type, key)["packageRecords"]
+
+    if len(pkgs) == 0:
+        print("> NO PACKAGES FOUND <\n")
+
+    print(f"\n{'':=<50}")
+    print(f"Found {len(pkgs)} packages, Applying filters")
+
+    with ThreadPoolExecutor() as pool:
+        for package in pkgs:
+            status = package["packageStatuses"]["status"]
+            package_id = package["packageId"]
+
+            if status == "DELIVERED":
+                print(f"--> Package {package_id} already delivered")
+                continue
+
+            pool.submit(
+                packages.mark_package_as_delivered, env, org_id, package_id, notes
+            )
+
+
+if __name__ == "__main__":
+    env = utils.select_env()
+    org_id = utils.select_org(env)
+
+    key = input(
+        "Type in the package number (e.g. 0510220444761000002, 00000009370592381919): "
+    )
+    key_type = input("Type in the package number type (e.g. bc, ori, pi, etc): ")
+
+    notes = input("Type in the notes (optional): ")
+
+    main(env, org_id, key_type, key, notes if notes else None)
